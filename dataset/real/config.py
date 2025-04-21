c3f25b49e267d15769893db7e5cd9b5b761f17ca
kingston-aditya <adityasarkar2000@outlook.com>
2025-04-21 17:52:29 +0000

    0iweoq

--- dataset/real/config.py
+++ dataset/real/config.py
@@ -2,14 +2,14 @@ def get_config():
     return {
         "repo_path": "/data/aditya/PRISM/",
         "input_data_dir": "/mnt/ssd/",
-        "llm_model": "Qwen/Qwen2.5-7B-Instruct",
-        "mllm_model": "Qwen/Qwen2.5-VL-7B-Instruct",
+        "llm_model": "Qwen/Qwen2.5-72B-Instruct",
+        "mllm_model": "Qwen/Qwen2.5-VL-72B-Instruct",
         "cache_dir": "/nfshomes/asarkar6/trinity/model_weights/",
-        "batch_size": 4,
+        "batch_size": 128,
         "dataloader_num_workers": 1,
         "is_sdxl": "True",
         "start_len": 0,
-        "end_len": 16,
+        "end_len": 512,
         "output_metadata_folder": "/mnt/ssd/finale_data/",
         "output_img_folder": "/mnt/ssd/finale_data/images/" 
     }
