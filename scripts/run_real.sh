902ed19c907de3a3be074373a8a27a17e328ce71
kingston-aditya <adityasarkar2000@outlook.com>
2025-04-29 02:29:36 -0400

    simplify bash

--- scripts/run_real.sh
+++ scripts/run_real.sh
@@ -1,14 +1,18 @@
+start_len = 0
+end_len = 75000
+job_id = 0
+
 python /nfshomes/asarkar6/aditya/PRISM/dataset/real/datagen_init.py \
- --start_len 0 \
- --end_len 750000 \
- --job_id 0
+ --start_len $start_len \
+ --end_len $end_len \
+ --job_id $job_id
 
 python /nfshomes/asarkar6/aditya/PRISM/dataset/real/run_llm.py \
- --start_len 0 \
- --end_len 750000 \
- --job_id 0
+ --start_len $start_len \
+ --end_len $end_len \
+ --job_id $job_id
 
 accelerate launch /nfshomes/asarkar6/aditya/PRISM/dataset/real/datagen_launch.py \
- --start_len 0 \
- --end_len 750000 \
- --job_id 0
+ --start_len $start_len \
+ --end_len $end_len \
+ --job_id $job_id
--- scripts/run_syn.sh
+++ scripts/run_syn.sh
@@ -1,9 +1,13 @@
+start_len = 0
+end_len = 75000
+job_id = 0
+
 python /nfshomes/asarkar6/aditya/PRISM/dataset/synthetic/run_llm.py \
- --start_len 0 \
- --end_len 750000 \
- --job_id 0
+ --start_len $start_len \
+ --end_len $end_len \
+ --job_id $job_id
 
 accelerate launch /nfshomes/asarkar6/aditya/PRISM/dataset/synthetic/run_final_syn.py \
- --start_len 0 \
- --end_len 750000 \
- --job_id 0
+ --start_len $start_len \
+ --end_len $end_len \
+ --job_id $job_id
