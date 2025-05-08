7280f71089dbac29a815951923c64292f72325ad
Vidyaranya <vidyaranya.ns@gmail.com>
2025-05-08 05:31:57 +0000

    Fix alignment bugs

--- dataset/utils.py
+++ dataset/utils.py
@@ -3,26 +3,34 @@ import numpy as np
 import json
 
 def correct_inputs(imgs, txts):
-    temp = {}
+    expanded_imgs_list = []
+    expanded_txts_list = []
     for i in range(len(txts)):
-        for j in txts[i].split(",")[:3]:
-            temp[j] = imgs[i]
-    return temp
+        for j in txts[i].split(","):
+            # temp[j] = imgs[i]
+            expanded_imgs_list.append(imgs[i])
+            expanded_txts_list.append(j)
+    return expanded_imgs_list, expanded_txts_list
 
 def GD_batcher(imgs, txts, batch_size):
     # create batches
-    t = {}; t1 = {}
+    t = []; t1 = []
     s = 0
     for i in range(len(txts)//batch_size):
-        t[i] = txts[batch_size*i:batch_size*(i+1)]
-        t1[i] = imgs[batch_size*i:batch_size*(i+1)]
+        # t[i] = txts[batch_size*i:batch_size*(i+1)]
+        t.append(txts[batch_size*i:batch_size*(i+1)])
+        # t1[i] = imgs[batch_size*i:batch_size*(i+1)]
+        t1.append(imgs[batch_size*i:batch_size*(i+1)])
         s+=1
 
     if len(txts)%batch_size != 0:
-        t[s] = txts[batch_size*(s):len(txts)]
-        t1[s] = imgs[batch_size*(s):len(imgs)]
+        # t[s] = txts[batch_size*(s):len(txts)]
+        t.append(txts[batch_size*(s):len(txts)])
+        # t1[s] = imgs[batch_size*(s):len(imgs)]
+        t1.append(imgs[batch_size*(s):len(imgs)])
 
-    return list(t.values()), list(t1.values())
+    # return list(t.values()), list(t1.values())
+    return t, t1
 
 def pretty_output(bbox_lst, filname_lst, noun_lst, cap_lst, f):
     k = 0
@@ -42,8 +50,10 @@ def pretty_output(bbox_lst, filname_lst, noun_lst, cap_lst, f):
                 atema.append({"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax, "labels": labels, "img_pth": filname})
             else:
                 pass
-        #import pdb; pdb.set_trace()
+
+        # import pdb; pdb.set_trace()
         temp = {"file_name": filname_lst[i], "prompt": cap_lst[i], "object": atema[:3]}
         k += end_idx
-        # f.write(json.dumps(temp) + '\n')
+        print(temp)
+        #f.write(json.dumps(temp) + '\n')
 
